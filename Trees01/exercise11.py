fe01b3acfba6a7059d10603baac5cae1f73184ac
emilyboo <ehbookstein@gmail.com>
2014-12-17 16:55:31 -0800

    Added breadth-first search using queue.

--- Trees01/exercise11.py
+++ Trees01/exercise11.py
@@ -23,6 +23,9 @@ class BinaryTreeNode:
         self.value = number
 
 def depth_first_traversal(node):
+    """
+    DFT, recursive
+    """
     print node.value
 
     # if a node to the left exists, go to it
@@ -34,6 +37,60 @@ def depth_first_traversal(node):
 
     # if neither of these conditions are met, function returns None
     print "done with this scope"
+
+def nr_depth_first_traversal(node):
+    """
+    Non-recursive depth first traversal
+    """
+    done = False
+
+    while not done:
+        print node.value
+        prev = node
+
+        if node.left:
+            node = node.left
+            continue
+
+        if node.right:
+            node = node.right
+            continue
+
+        else:
+            node = prev
+
+def breadth_first_search(node):
+    if not node:
+        return None
+
+    else:
+        queue = [node]
+        while len(queue) > 0:
+            current = queue[0]
+            print current.value
+
+            if current.left:
+                queue.append(current.left)
+            if current.right:
+                queue.append(current.right)
+
+            queue.pop(0)
+
+
+root = BinaryTreeNode(0)
+left = BinaryTreeNode(1)
+right = BinaryTreeNode(4)
+l_left = BinaryTreeNode(2)
+l_right = BinaryTreeNode(3)
+r_left = BinaryTreeNode(5)
+r_right= BinaryTreeNode(6)
+root.set_left(left)
+root.set_right(right)
+left.set_left(l_left)
+left.set_right(l_right)
+right.set_left(r_left)
+right.set_right(r_right)
+
 
 
 
